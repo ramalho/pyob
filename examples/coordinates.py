defd533be24345b4f219b7be216dd0b8f19c4fb3
Luciano Ramalho <luciano@ramalho.org>
2019-04-21 23:12:25 -0300

    class vs instance attributes

---
+++ examples/coordinates.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class Coordinate:
+    lat: float
+    long: float
+
+    def __str__(self):
+        ns = 'NS'[self.lat < 0]
+        we = 'WE'[self.long < 0]
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.long):.1f}°{we}'
+
+
+sp = Coordinate(-23, 46)
+
+print(sp)
+
+sp.lat = 40
+
+print(sp)
